8be356ae9f5e86ad88182950387fce7e99c8523a
Alejandro <a.guerrero.penalver@gmail.com>
2023-03-20 11:16:56 +0100

    Adding OpenAPI and Swagger configuration

--- Client_Server/src/main/java/com/gpalex/clientserver/ClientserverApplication.java
+++ Client_Server/src/main/java/com/gpalex/clientserver/ClientserverApplication.java
@@ -8,37 +8,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @SpringBootApplication
 @RestController
 public class ClientserverApplication {
-	
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientserverApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ClientserverApplication.class, args);
+  }
 
-    //@Value("${user.roleprop}")
-    @Value("${user.role}")
-    private String role;
+  // @Value("${user.roleprop}")
+  @Value("${user.role}")
+  private String role;
 
-    @Value("${user.pwd}")
-    private String pwd;
+  @Value("${user.pwd}")
+  private String pwd;
 
-  
-    @GetMapping(
-      value = "/whoami/{username}",  
-      produces = MediaType.TEXT_PLAIN_VALUE)
-    public String whoami(@PathVariable("username") String username) {
-        return String.format("Hello! You're %s and you'll become a(n) %s with pwd %s...\n", username, role, pwd);
-    }
+  @Operation(summary = "Retrieve user and password")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Found the credentials", content = @Content),
+      @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Book not found", content = @Content) }) // @formatter:on
 
-    @GetMapping(
-      value = "/whoami/hello",  
-      produces = MediaType.TEXT_PLAIN_VALUE)
-    public String hello() {
-        return String.format("Hello");
-    }
-	
-	
+  @GetMapping(value = "/{username}", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String whoami(@PathVariable("username") String username) {
+    return String.format("Hello! You're %s and you'll become a(n) %s with pwd %s...\n", username, role, pwd);
+  }
+
+  @Operation(summary = "Say Hello")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Hello", content = @Content),
+      @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Book not found", content = @Content) }) // @formatter:on
+
+  @GetMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String hello() {
+    return String.format("Hello");
+  }
+
 }
